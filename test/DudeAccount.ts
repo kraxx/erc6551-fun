135e0ad187a498cde2f417199f54734cb3e3c58d
Justin Chow <justin@onomato.xyz>
2023-05-27 23:35:35 -0400

    cleanup unused references; delete Lock

--- test/DudeAccount.ts
+++ test/DudeAccount.ts
@@ -1,9 +1,8 @@
-import { time, loadFixture } from "@nomicfoundation/hardhat-network-helpers";
-import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
+import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
 import { expect } from "chai";
 import { ethers } from "hardhat";
 
-describe("E2E", function () {
+describe("DudeAccount", function () {
   // We define a fixture to reuse the same setup in every test.
   // We use loadFixture to run this setup once, snapshot that state,
   // and reset Hardhat Network to that snapshot in every test.
--- test/HatToken.ts
+++ test/HatToken.ts
@@ -1,7 +1,6 @@
 import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
 import { expect } from "chai";
 import { ethers } from "hardhat";
-import { token } from "../typechain-types/@openzeppelin/contracts";
 
 describe("HatToken", function () {
   // We define a fixture to reuse the same setup in every test.
